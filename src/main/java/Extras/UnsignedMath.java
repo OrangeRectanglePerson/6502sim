ecf692c0aec77aa4db570328e695549e3cd095f7
OrangeRectanglePerson <orangerectangleperson@gmail.com>
2022-08-25 00:00:15 +0800

    Added comments to UnsignedMath
    just use the normal +- operators from nowon
    
    binary of signed a +- signed b == binary of unsigned a +- unsigned b

--- src/main/java/Extras/UnsignedMath.java
+++ src/main/java/Extras/UnsignedMath.java
@@ -3,10 +3,14 @@ package Extras;
 public class UnsignedMath {
     //Unsigned Math Class for unsigned stuff
 
+    //whoops i apparently didnt need addByte and addShort
+    //binary of signed a + signed b == binary of unsigned a + unsigned b
     public static byte addByte(byte a, byte b){ return (byte) (Byte.toUnsignedInt(a) + Byte.toUnsignedInt(b));}
 
     public static short addShort(short a, short b){ return (short) (Short.toUnsignedInt(a) + Short.toUnsignedInt(b));}
 
+    //this part is still very useful though
+    //this is because (short) byte_value will maintain sign bit
     public static short byteToShort(byte low_byte) {
         //default high byte to 0x00
         return (short) (Byte.toUnsignedInt(low_byte));
